930bb5c6162a49c7bcde99af73c8e6b32ebfea90
Jean-Charles <jeancharles.cano@outlook.fr>
2021-06-04 09:22:55 +0200

    ajout de toutes les dépendances, et création du job Producter

---
+++ txData/CFXDefault_A8F640.base/resources/[jobs]/esx_producterjob/config.lua
@@ -0,0 +1,68 @@
+Config                            = {}
+Config.DrawDistance               = 100.0
+Config.MaxInService               = -1
+Config.EnablePlayerManagement     = true
+Config.EnableSocietyOwnedVehicles = false
+Config.Locale                     = 'fr'
+
+Config.Zones = {
+
+	PotatoFarm = {
+		Pos   = {x = 375.31, y = 6472.89, z = 28.92},
+		Size  = {x = 3.5, y = 3.5, z = 2.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Ramassage des patates",
+		Type  = 1
+	},
+
+
+	TraitementPotato = {
+		Pos   = {x = 461.82, y = 6531.83, z = 27.72},
+		Size  = {x = 3.5, y = 3.5, z = 2.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "nettoyage et traitement des patates",
+		Type  = 1
+	},
+	
+	SellFarm = {
+		Pos   = {x = 1379.43, y = 3623.61, z = 33.89},
+		Size  = {x = 3.5, y = 3.5, z = 2.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Vente des patates fraiches",
+		Type  = 1
+	},
+
+	ProducteurActions = {
+		Pos   = {x = 416.63, y = 6520.68, z = 26.71},
+		Size  = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Point d'action",
+		Type  = 0
+	 },
+	  
+	VehicleSpawner = {
+		Pos   = {x = 408.19, y = 6497.11, z = 26.82},
+		Size = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Garage véhicule",
+		Type  = 0
+	},
+
+	VehicleSpawnPoint = {
+		Pos   = {x = 410.12, y = 6481.56, z = 27.81},
+		Size  = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Spawn point",
+		Type  = -1
+	},
+
+	VehicleDeleter = {
+		Pos   = {x = 419.72, y = 6476.32, z = 27.81},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Ranger son véhicule",
+		Type  = 0
+	}
+
+}
+
